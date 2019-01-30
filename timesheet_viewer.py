165ce531b7d6522d614d642df88b45d79b229cd2
Colin Martin <colin.martin@gmail.com>
2019-01-30 22:17:28 +0000

    Implement initial QDatepicker in replacement of QCalender

--- timesheet_viewer.py
+++ timesheet_viewer.py
@@ -1,20 +1,33 @@
 import sys
 
-from PySide2.QtWidgets import QDialog, QGridLayout, QCalendarWidget, QApplication
+from PySide2.QtCore import QDate
+from PySide2.QtWidgets import QDialog, QGridLayout, QCalendarWidget, QApplication, QLabel, QDateEdit
 
 
 class Viewer(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         grid_layout = QGridLayout(self)
-        self.cal = Calendar()
+        self.date_picker = DatePicker()
+        # self.cal.clicked.connect(self.show_date)
+        self.date_label = QLabel()
+        self.date_label.setText("Some Text")
+        grid_layout.addWidget(self.date_label, 0, 0)
+        grid_layout.addWidget(self.date_picker, 1, 0)
 
-        grid_layout.addWidget(self.cal)
+    def show_date(self, date):
+        self.date_label.setText(date.toString())
 
+
+class DatePicker(QDateEdit):
+    def __init__(self):
+        super().__init__()
+        self.setDate(QDate.currentDate())
 
 class Calendar(QCalendarWidget):
     def __init__(self):
         super().__init__()
+        self.setGridVisible(True)
 
 
 if __name__ == "__main__":
